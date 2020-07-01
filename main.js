d3d6e17acb46991a1718585ee029b5c4944a1796
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-07-01 12:46:15 -0300

    Set initial window configs

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  process.platform !== 'darwin' && app.quit()
+})
+
+app.on('activate', () => {
+  !BrowserWindow.getAllWindows().length && createWindow()
+})
